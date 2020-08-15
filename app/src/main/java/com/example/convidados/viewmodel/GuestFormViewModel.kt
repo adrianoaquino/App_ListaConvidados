d4a2611d2fc7478e428d2cab68e745f593fb02d3
adrianoaquino <56889327+adrianoaquino@users.noreply.github.com>
2020-08-15 01:49:12 -0300

    commit

---
+++ app/src/main/java/com/example/convidados/viewmodel/GuestFormViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.convidados.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel : ViewModel() {
+
+    private var mSaveGuest = MutableLiveData<Boolean>()
+    val saveGuest: LiveData<Boolean> = mSaveGuest
+
+    fun save(name: String, presence: Boolean) {
+
+     }
+
+}
